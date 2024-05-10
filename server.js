461c55049a6806b0f8a60700bbd28cc34b76edb3
Ludo <l.qerraxhija@gmail.com>
2024-05-10 11:01:12 +0200

    Using express, adedd routers

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const http = require('http');
 
 // create http server
-const server = http.createServer((req, res) => {
-    // Set the response headers
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h1>Hello. Node.js HTTP Server!</h1>');
-    res.end();
+const express = require('express');
+const app = express();
+
+// Include router files
+const usersRoute = require('./routes/users');
+const notesRoute = require('./routes/notes');
+
+app.get('/', (req, res) => {
+    res.send('Hello root note');
 });
+
+app.use('/users', usersRoute);
+app.use('/notes', notesRoute);
 
 const port = 3000;
 
-//Start the server
-server.listen(port, () => {
-    console.log(`Node.js HTTP server is running on port ${port}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
